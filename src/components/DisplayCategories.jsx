b45841ee81bf2d1095725aa5f386b90f7f2d62a1
EddySafc <eddy@eddy-730QED>
2022-12-08 16:15:59 +0000

    add sorting buttons for reviews

--- src/components/DisplayCategories.jsx
+++ src/components/DisplayCategories.jsx
@@ -2,33 +2,118 @@ import { getReviews } from "../requests";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
+import moment from "moment";
 
 const DisplayCategories = ({ chosenCategory }) => {
   const [categoryReviews, setCategoryReviews] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [sort, setSort] = useState(null);
+
   useEffect(() => {
     getReviews().then((allReviews) => {
-      setCategoryReviews(
-        allReviews.filter((review) => {
-          return review.category === chosenCategory;
-        }),
-        setLoading(false)
-      );
+      let categoryFilter = allReviews.filter((review) => {
+        return review.category === chosenCategory;
+      });
+      if (sort === "newest-oldest" || sort === null) {
+        setCategoryReviews(
+          [...categoryFilter].sort(
+            (a, b) =>
+              moment(b.created_at).format("YYYYMMDDHHmmss") -
+              moment(a.created_at).format("YYYYMMDDHHmmss")
+          )
+        );
+
+        setLoading(false);
+      }
+      if (sort === "oldest-newest") {
+        setCategoryReviews(
+          [...categoryFilter].sort(
+            (a, b) =>
+              moment(a.created_at).format("YYYYMMDDHHmmss") -
+              moment(b.created_at).format("YYYYMMDDHHmmss")
+          )
+        );
+      }
+      if (sort === "votes_asc") {
+        setCategoryReviews(
+          [...categoryFilter].sort((a, b) => a.votes - b.votes)
+        );
+      }
+      if (sort === "votes_desc") {
+        setCategoryReviews(
+          [...categoryFilter].sort((a, b) => b.votes - a.votes)
+        );
+      }
+      if (sort === "comments_asc") {
+        setCategoryReviews(
+          [...categoryFilter].sort((a, b) => a.comment_count - b.comment_count)
+        );
+      }
+      if (sort === "comments_desc") {
+        setCategoryReviews(
+          [...categoryFilter].sort((a, b) => b.comment_count - a.comment_count)
+        );
+      }
     });
-  }, [chosenCategory]);
+  }, [sort, chosenCategory]);
 
   if (loading === true) {
     return <section>Loading...</section>;
   }
   if (loading === false) {
     return (
-      <section className="specific_category_review">
+      <section>
+        Sort By:
+        <button
+          onClick={() => {
+            setSort("newest-oldest");
+          }}
+        >
+          newest to oldest
+        </button>
+        <button
+          onClick={() => {
+            setSort("oldest-newest");
+          }}
+        >
+          oldest to newest
+        </button>
+        <button
+          onClick={() => {
+            setSort("votes_asc");
+          }}
+        >
+          votes ascending
+        </button>
+        <button
+          onClick={() => {
+            setSort("votes_desc");
+          }}
+        >
+          votes Descending
+        </button>
+        <button
+          onClick={() => {
+            setSort("comments_asc");
+          }}
+        >
+          comment count ascending
+        </button>
+        <button
+          onClick={() => {
+            setSort("comments_desc");
+          }}
+        >
+          comment count Descending
+        </button>
         {categoryReviews.map((review) => {
           return (
-            <ul>
+            <ul className="specific_category_review">
               <li key={review.review_id} id="specific_category_review">
                 <p>{review.title}</p>
-                <p>{review.owner}</p>
+                <p>Votes:{review.votes}</p>
+                <p>Comment Count:{review.comment_count}</p>
+                <p>{moment(review.created_at).format(`DD/MM/YY [at] HH:mm`)}</p>
                 <img id="review-image" src={review.review_img_url}></img>
                 <p>
                   <Link to={`/reviews/${review.review_id}`}>
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,15 +2,46 @@ import { useEffect } from "react";
 import { getReviews } from "../requests";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import moment from "moment";
 
 const Reviews = ({ reviews, setReviews }) => {
   const [loading, setLoading] = useState(true);
+  const [sort, setSort] = useState(null);
   useEffect(() => {
     getReviews().then((data) => {
-      setReviews(data);
-      setLoading(false);
+      if (sort === "newest-oldest" || sort === null) {
+        setReviews(
+          [...data].sort(
+            (a, b) =>
+              moment(b.created_at).format("YYYYMMDDHHmmss") -
+              moment(a.created_at).format("YYYYMMDDHHmmss")
+          )
+        );
+        setLoading(false);
+      }
+      if (sort === "oldest-newest") {
+        setReviews(
+          [...data].sort(
+            (a, b) =>
+              moment(a.created_at).format("YYYYMMDDHHmmss") -
+              moment(b.created_at).format("YYYYMMDDHHmmss")
+          )
+        );
+      }
+      if (sort === "votes_asc") {
+        setReviews([...data].sort((a, b) => a.votes - b.votes));
+      }
+      if (sort === "votes_desc") {
+        setReviews([...data].sort((a, b) => b.votes - a.votes));
+      }
+      if (sort === "comments_asc") {
+        setReviews([...data].sort((a, b) => a.comment_count - b.comment_count));
+      }
+      if (sort === "comments_desc") {
+        setReviews([...data].sort((a, b) => b.comment_count - a.comment_count));
+      }
     });
-  }, []);
+  }, [sort]);
 
   if (loading === true) {
     return <section>Loading...</section>;
@@ -18,12 +49,58 @@ const Reviews = ({ reviews, setReviews }) => {
   if (loading === false) {
     return (
       <section>
+        Sort By:
+        <button
+          onClick={() => {
+            setSort("newest-oldest");
+          }}
+        >
+          newest to oldest
+        </button>
+        <button
+          onClick={() => {
+            setSort("oldest-newest");
+          }}
+        >
+          oldest to newest
+        </button>
+        <button
+          onClick={() => {
+            setSort("votes_asc");
+          }}
+        >
+          votes ascending
+        </button>
+        <button
+          onClick={() => {
+            setSort("votes_desc");
+          }}
+        >
+          votes Descending
+        </button>
+        <button
+          onClick={() => {
+            setSort("comments_asc");
+          }}
+        >
+          comment count ascending
+        </button>
+        <button
+          onClick={() => {
+            setSort("comments_desc");
+          }}
+        >
+          comment count Descending
+        </button>
         <ul>
           {reviews.map((review) => {
             return (
               <li className="listed-review" key={review.review_id}>
-                <p>{review.title}</p>
-                <p>{review.owner}</p>
+                <p>Title:{review.title}</p>
+
+                <p>Votes:{review.votes}</p>
+                <p>Comment Count:{review.comment_count}</p>
+                <p>{moment(review.created_at).format(`DD/MM/YY [at] HH:mm`)}</p>
                 <img id="review-image" src={review.review_img_url}></img>
                 <p>
                   <Link to={`/reviews/${review.review_id}`}>
